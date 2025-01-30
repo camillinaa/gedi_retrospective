9a698224c5a9dd1fd3afaae67342b6810c7d76f3
Camilla Callierotti <camillac23@gmail.com>
2025-01-30 12:14:52 +0100

    edited script to create columns independently as dictionaries

--- cnv_hotspot_mapping.py
+++ cnv_hotspot_mapping.py
@@ -53,43 +53,58 @@ def match_cnvs_to_hotspots(cnvs_dir, hotspots_path, skip_list):
                                 "percent_overlap": overlap/(hotspot_range[1]-hotspot_range[0])*100
                                 # add quality metrics
                             })
-    return cnvs_hotspots_matched
-
-# add total percentage overlap for all cnvs covering the same hotspot in each sample
-
-def add_total_percentage_overlap(matched_dict):
-    matched_df = pd.DataFrame(matched_dict)
-    matched_df["percent_overlap_total"] = matched_df.groupby(["sample", "chromosome", "hotspot_start", "hotspot_end"])["percent_overlap"].transform("sum")
+    matched_df = pd.DataFrame(cnvs_hotspots_matched)
     return matched_df
 
-# add blacklist/gap/lowsignal regions
+# compute total percentage overlap for all cnvs covering the same hotspot in each sample (output: dictionary)
 
-def add_genome_regions(mapped_list, blacklist_bed, gapregions_bed):
+def get_total_overlap(matched_df):
+    total_overlap_dict = matched_df.groupby(["sample", "chromosome", "hotspot_start", "hotspot_end"])["percent_overlap"].sum().to_dict()
+    return total_overlap_dict
+
+# compute blacklist/gap/lowsignal regions (output: dictionary)
+
+def get_genome_regions(matched_df, blacklist_bed, gapregions_bed):
     blacklist_df = pd.read_csv(blacklist_bed, sep="\t", names=["Chromosome", "Start", "End", "Motivation"], header=None)
-    updated_list = []
+    genome_regions_dict = {}
     #gapregions_df = pd.read_csv(gapregions_bed, sep="\t", names=["Chromosome", "Start", "End"], header=None)
-    for call in mapped_list:
+    for _, call in matched_df.iterrows():
         cnv_range = [call["cnv_start"], call["cnv_end"]]
-        genome_regions = set()
+        regions = set()
         for _, region in blacklist_df.iterrows():
             blacklist_range = [region["Start"], region["End"]]
             overlap = get_overlap(cnv_range, blacklist_range)
             if overlap > 0:
-                genome_regions.add(region["Motivation"])
-        call["genome_region"] = ", ".join(genome_regions) if genome_regions else "None"
-        updated_list.append(call)
-    return updated_list
+                regions.add(region["Motivation"])
+        genome_regions_dict[call["sample"]] = ", ".join(regions) if regions else "None"
+    return genome_regions_dict
 
 # apply function to all files in directory
 
-not_included = ["A549.markdup.bam_CNVs"] # define samples to exclude
-cnv_hotspot_match = match_cnvs_to_hotspots(cnv_calls_dir, Common_Abnormal_Regions_path, not_included)
-cnv_hotspot_match_total_percent = add_total_percentage_overlap(cnv_hotspot_match)
-cnv_hotspot_match_genome_regions = add_genome_regions(cnv_hotspot_match_total_percent, blacklist_bed, gapregions_bed)
+def main():
+    not_included = ["A549.markdup.bam_CNVs"] # define samples to exclude
 
-# save excel
+    cnv_hotspot_match = match_cnvs_to_hotspots(cnv_calls_dir, Common_Abnormal_Regions_path, not_included)
+    
+    total_overlap = get_total_overlap(cnv_hotspot_match)
+    genome_regions = get_genome_regions(cnv_hotspot_match, blacklist_bed, gapregions_bed)
 
-writer = pd.ExcelWriter('cnvs_hotspots_mapped.xlsx', engine='xlsxwriter')
-final_df.to_excel(writer, sheet_name='Sheet1')
-writer.save()
+    final_df = cnv_hotspot_match.assign(
+        total_overlap=cnv_hotspot_match.apply(
+            lambda row: total_overlap.get(
+                (row["sample"], row["chromosome"], row["hotspot_start"], row["hotspot_end"]), 0
+            ), axis=1
+        ),
+        genome_region=cnv_hotspot_match["sample"].map(genome_regions)
+    )
 
+    writer = pd.ExcelWriter('cnvs_hotspots_mapped.xlsx', engine='xlsxwriter')
+    final_df.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+
+    return final_df
+
+
+
+if __name__ == "__main__":
+    final_result=main()
